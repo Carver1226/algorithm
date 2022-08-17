73dd302d0138ae79cf1be16a4aa54d3f351746cd
Carver1226 <105765336+Carver1226@users.noreply.github.com>
2022-08-17 23:45:04 +0800

    Update main.go

--- algorithm/quickSort/main.go
+++ algorithm/quickSort/main.go
@@ -1,35 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func quickSort(arr []int, left int, right int) {
 	if left >= right {
 		return
 	}
 	x := arr[(left + right) / 2]
-	l := left
-	r := right
+	l := left - 1
+	r := right + 1
 	for l < r {
+		l++
 		for arr[l] < x {
 			l++
 		}
+		r--
 		for arr[r] > x {
 			r--
 		}
 		if l < r {
-			arr[l], arr[r] = swap(arr[l], arr[r])
+			arr[l], arr[r] = arr[r], arr[l]
 		}
 	}
 	quickSort(arr, left, r)
 	quickSort(arr, r + 1, right)
 }
 
-func swap(a int, b int) (int, int){
-	return b, a
+func qSort(arr []int) {
+	quickSort(arr, 0, len(arr) - 1)
 }
 
 func main() {
-	arr := []int{2, 5, 6, 1, 4, 8}
-	quickSort(arr, 0, 5)
-	fmt.Printf("%v", arr)
+	arr := []int{2, 2}
+	qSort(arr)
+	fmt.Printf("%v\n", arr)
 }
